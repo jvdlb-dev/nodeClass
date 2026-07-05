4b2bbdadc92832a664de8505342d4d8338e054fe
jvdlb6 <jvdlb6@gmail.com>
2026-07-04 23:48:29 -0300

    update server to handle JSON body parsing for POST requests

--- 01-fundamentos-nodejs/streams/stream-http-server.js
+++ 01-fundamentos-nodejs/streams/stream-http-server.js
@@ -13,13 +13,6 @@ class InverseNumberStream extends Transform {
 
 
 const server = http.createServer(async (req, res) => {
-  const buffers = []
-
-  for await (const chunk of req) {
-    buffers.push(chunk)
-  }
-
-  const fullStreamContent = Buffer.concat(buffers).toString()
   console.log(fullStreamContent)
 
   return res.end(fullStreamContent)
