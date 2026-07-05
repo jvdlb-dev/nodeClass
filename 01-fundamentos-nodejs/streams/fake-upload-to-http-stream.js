f32bb9f2a1460c1887398e32d68765bd5cdf50d2
jvdlb6 <jvdlb6@gmail.com>
2026-07-04 22:30:04 -0300

    http and server basics

---
+++ 01-fundamentos-nodejs/streams/fake-upload-to-http-stream.js
@@ -0,0 +1,25 @@
+import { Readable } from 'node:stream'
+
+class OnetoHundredStream extends Readable {
+    index = 1
+    
+    _read() {
+        const i = this.index++
+
+       setTimeout(() => {
+            if (i > 100) {
+                this.push(null)
+            } else {
+                const buf = Buffer.from(String(i))
+                this.push(buf)
+            }
+        }, 10)
+    }
+}
+
+fetch('http://localhost:3334', {
+    method: 'POST',
+    body: new OnetoHundredStream(),
+    duplex: 'half'
+})
+    
