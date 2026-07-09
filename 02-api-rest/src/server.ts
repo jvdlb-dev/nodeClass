9d0dc721be8591e78f85d7b2ed59acbad637a867
jvdlb6 <jvdlb6@gmail.com>
2026-07-09 15:29:50 -0300

    add transaction routes with session validation and cookie handling

--- 02-api-rest/src/server.ts
+++ 02-api-rest/src/server.ts
@@ -1,10 +1,15 @@
 import fastify from 'fastify'
 import { env } from './env/index.js'
 import { transactionRoutes } from './routes/transactions.js'
+import cookie from '@fastify/cookie'
 
 const app = fastify()
 
-app.register(transactionRoutes)
+app.register(cookie)
+
+app.register(transactionRoutes, {
+  prefix: 'transactions',
+})
 
 app
   .listen({
