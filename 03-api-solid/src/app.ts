4d2c84ec75cbea43229fa6ed24a810d117a339ef
jvdlb6 <jvdlb6@gmail.com>
2026-07-20 00:04:25 -0300

    remove unused Prisma client setup and add user registration controller

--- 03-api-solid/src/app.ts
+++ 03-api-solid/src/app.ts
@@ -1,19 +1,6 @@
 import fastify from 'fastify'
-import { PrismaClient } from '../generated/prisma/client'
-import { PrismaPg } from '@prisma/adapter-pg'
+import { appRoutes } from './http/routes'
 
 export const app = fastify()
 
-if (!process.env.DATABASE_URL) {
-  throw new Error('DATABASE_URL is not set')
-}
-
-const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL })
-const prisma = new PrismaClient({ adapter })
-
-prisma.user.create({
-  data: {
-    name: 'John Doe',
-    email: 'john.doe@example.com',
-  },
-})
+app.register(appRoutes)
