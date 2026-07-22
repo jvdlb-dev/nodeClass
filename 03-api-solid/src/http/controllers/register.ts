fec56731e1d25a2b5168c1be6999ba88ad91f881
jvdlb6 <jvdlb6@gmail.com>
2026-07-22 00:05:38 -0300

    implement user registration use case with error handling and repository pattern

--- 03-api-solid/src/http/controllers/register.ts
+++ 03-api-solid/src/http/controllers/register.ts
@@ -1,6 +1,8 @@
 import z from 'zod'
-import { prisma } from '@/lib/prisma'
 import type { FastifyReply, FastifyRequest } from 'fastify'
+import { RegisterUseCase } from '@/use-cases/register'
+import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
+import { UserAlreadyExistsError } from '@/use-cases/errors/user-already-exists'
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
   const registerBodySchema = z.object({
@@ -11,13 +13,16 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
 
   const { name, email, password } = registerBodySchema.parse(request.body)
 
-  await prisma.user.create({
-    data: {
-      name,
-      email,
-      password_hash: password,
-    },
-  })
+  try {
+    const usersRepository = new PrismaUsersRepository()
+    const registerUseCase = new RegisterUseCase(usersRepository)
 
+    await registerUseCase.execute({ name, email, password })
+  } catch (err) {
+    if (err instanceof UserAlreadyExistsError) {
+      return reply.status(409).send({ message: err.message })
+    }
+    throw err
+  }
   return reply.status(201).send()
 }
