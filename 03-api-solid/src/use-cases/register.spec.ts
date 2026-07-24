b4c7a1722dee517d43f14c48ebd47f29f668f5d9
jvdlb6 <jvdlb6@gmail.com>
2026-07-24 01:18:28 -0300

    add tests for register use case to validate password hashing and email uniqueness

---
+++ 03-api-solid/src/use-cases/register.spec.ts
@@ -0,0 +1,63 @@
+import { describe, expect, it } from 'vitest'
+import { RegisterUseCase } from './register'
+import { compare } from 'bcryptjs'
+import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository'
+import { UserAlreadyExistsError } from './errors/user-already-exists'
+
+describe('Register Use Case', () => {
+  it('should hash user password upon registration', async () => {
+    const usersRepository = new InMemoryUsersRepository()
+    const registerUseCase = new RegisterUseCase(usersRepository)
+
+    const { user } = await registerUseCase.execute({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: 'password123',
+    })
+
+    const isPasswordCorrectlyHashed = await compare(
+      'password123',
+      user.password_hash,
+    )
+
+    expect(isPasswordCorrectlyHashed).toBe(true)
+  })
+
+  it('should not be able to register with an existing email', async () => {
+    const usersRepository = new InMemoryUsersRepository()
+    const registerUseCase = new RegisterUseCase(usersRepository)
+    const email = 'johndoe@example.com'
+
+    await registerUseCase.execute({
+      name: 'John Doe',
+      email,
+      password: 'password123',
+    })
+
+    expect(() => {
+      return registerUseCase.execute({
+        name: 'John Doe',
+        email,
+        password: 'password123',
+      })
+    }).rejects.toBeInstanceOf(UserAlreadyExistsError)
+  })
+
+  it('should be able to register a new user', async () => {
+    const usersRepository = new InMemoryUsersRepository()
+    const registerUseCase = new RegisterUseCase(usersRepository)
+
+    const { user } = await registerUseCase.execute({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: 'password123',
+    })
+
+    expect(user).toEqual(
+      expect.objectContaining({
+        name: 'John Doe',
+        email: 'johndoe@example.com',
+      }),
+    )
+  })
+})
