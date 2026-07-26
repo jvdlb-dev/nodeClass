a85cdae1caec975af45170c5d7fc9a5aafee67b1
jvdlb6 <jvdlb6@gmail.com>
2026-07-26 18:38:48 -0300

    add tests for GetUserProfileUseCase to validate user retrieval and error handling

---
+++ 03-api-solid/src/use-cases/spec/get-user-profile.spec.ts
@@ -0,0 +1,37 @@
+import { beforeEach, describe, expect, it } from 'vitest'
+import { hash } from 'bcryptjs'
+import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository'
+import { GetUserProfileUseCase } from '../get-user-profile'
+import { ResourceNotFoundError } from '../errors/resource-not-found-error'
+
+let usersRepository: InMemoryUsersRepository
+let sut: GetUserProfileUseCase
+
+describe('Get user profile Use Case', () => {
+  beforeEach(() => {
+    usersRepository = new InMemoryUsersRepository()
+    sut = new GetUserProfileUseCase(usersRepository)
+  })
+
+  it('should be able to get user profile', async () => {
+    await usersRepository.create({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password_hash: await hash('123456', 6),
+    })
+
+    const { user } = await sut.execute({
+      userId: 'user-1',
+    })
+
+    expect(user.id).toEqual(expect.any(String))
+  })
+
+  it('should not be able to get user profile with wrong ID', async () => {
+    await expect(() => {
+      return sut.execute({
+        userId: 'non-existent-user',
+      })
+    }).rejects.toBeInstanceOf(ResourceNotFoundError)
+  })
+})
