7fc2759eca20a06c2801c724d5442b735c7d225c
jvdlb6 <jvdlb6@gmail.com>
2026-07-26 18:37:50 -0300

    create spec folder

--- 03-api-solid/src/use-cases/spec/register.spec.ts
+++ 03-api-solid/src/use-cases/spec/register.spec.ts
@@ -1,8 +1,8 @@
 import { beforeEach, describe, expect, it } from 'vitest'
-import { RegisterUseCase } from './register'
+import { RegisterUseCase } from '../register'
 import { compare } from 'bcryptjs'
 import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository'
-import { UserAlreadyExistsError } from './errors/user-already-exists-error'
+import { UserAlreadyExistsError } from '../errors/user-already-exists-error'
 
 let usersRepository: InMemoryUsersRepository
 let sut: RegisterUseCase
