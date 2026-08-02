039f25b792e05102baca2a2e1f4d446039a92252
jvdlb6 <jvdlb6@gmail.com>
2026-08-02 16:10:37 -0300

    use created user ID for fetching user profile in tests

--- 03-api-solid/src/use-cases/spec/get-user-profile.spec.ts
+++ 03-api-solid/src/use-cases/spec/get-user-profile.spec.ts
@@ -14,17 +14,18 @@ describe('Get user profile Use Case', () => {
   })
 
   it('should be able to get user profile', async () => {
-    await usersRepository.create({
+    const createdUser = await usersRepository.create({
       name: 'John Doe',
       email: 'johndoe@example.com',
       password_hash: await hash('123456', 6),
     })
 
     const { user } = await sut.execute({
-      userId: 'user-1',
+      userId: createdUser.id,
     })
 
     expect(user.id).toEqual(expect.any(String))
+    expect(user.name).toEqual('John Doe')
   })
 
   it('should not be able to get user profile with wrong ID', async () => {
